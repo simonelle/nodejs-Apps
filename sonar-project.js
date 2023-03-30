01e551f2b49566d841632dee4a506357dd6ec7d7
simonelle <97528755+simonelle@users.noreply.github.com>
2023-03-30 12:13:54 -0400

    updated files

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://172.31.7.169:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.login': '6b8b1a624aace88638531980a5e215b2aea48e56',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
